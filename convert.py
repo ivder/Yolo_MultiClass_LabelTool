b7843f5af9d5bc210d9c48e56206492db4343ca5
Ivan Derianto <32887298+ivder@users.noreply.github.com>
2019-11-26 13:20:36 +0900

    Update convert.py

--- convert.py
+++ convert.py
@@ -4,7 +4,6 @@ import os
 from os import walk, getcwd
 from PIL import Image
 
-classes = ["pothole","spalling","spallingcross","patchdamaged"]
 
 def convert(size, box):
     dw = 1./size[0]
@@ -19,82 +18,92 @@ def convert(size, box):
     h = h*dh
     return (x,y,w,h)
     
-    
-"""-------------------------------------------------------------------""" 
 
-""" Configure Paths"""   
-mypath = "./Labels/1/"
-outpath = "./Labels/output/"
-
-project = "1"
-
-wd = getcwd()
-list_file = open('%s/%s_list.txt'%(wd, project), 'w')
-
-""" Get input text file list """
-txt_name_list = []
-for (dirpath, dirnames, filenames) in walk(mypath):
-    txt_name_list.extend(filenames)
-    break
-# print(txt_name_list)
-
-""" Process """
-for txt_name in txt_name_list:
-    # txt_file =  open("Labels/stop_sign/001.txt", "r")
+def Convert2Yolo(mypath, outpath, project, classes):
+    wd = getcwd()
+    list_file = open('%s/log/%s_list.txt'%(wd, project), 'w')
     
-    """ Open input text files """
-    txt_path = mypath + txt_name
-    print("Input:" + txt_path)
-    txt_file = open(txt_path, "r")
-    lines = txt_file.read().split('\n')   #for ubuntu, use "\r\n" instead of "\n"
+    """ Get input text file list """
+    txt_name_list = []
+    for (dirpath, dirnames, filenames) in walk(mypath):
+        txt_name_list.extend(filenames)
+        break
+    # print(txt_name_list)
     
-    """ Open output text files """
-    txt_outpath = outpath + txt_name
-    print("Output:" + txt_outpath)
-    txt_outfile = open(txt_outpath, "a")
-    
-    
-    """ Convert the data to YOLO format """
-    ct = 0
-    for line in lines:
-        #print('lenth of line is: ')
-        #print(len(line))
-        #print('\n')
-        elems = line.split(' ')
-        if(len(elems) >= 2):
-        #if(len(line) >= 2):
-            ct = ct + 1
-            print(line + "\n")
+    """ Process """
+    for txt_name in txt_name_list:
+        # txt_file =  open("Labels/stop_sign/001.txt", "r")
+        
+        """ Open input text files """
+        txt_path = mypath + txt_name
+        print("Input:" + txt_path)
+        txt_file = open(txt_path, "r")
+        lines = txt_file.read().split('\n')   #for ubuntu, use "\r\n" instead of "\n"
+        
+        """ Open output text files """
+        if not os.path.exists(outpath):
+            os.mkdir(outpath)
+        txt_outpath = outpath + txt_name
+        print("Output:" + txt_outpath)
+        txt_outfile = open(txt_outpath, "w")
+        
+        
+        """ Convert the data to YOLO format """
+        ct = 0
+        for line in lines:
+            #print('lenth of line is: ')
+            #print(len(line))
+            #print('\n')
             elems = line.split(' ')
-            print(elems)
-            xmin = elems[0]
-            xmax = elems[2]
-            ymin = elems[1]
-            ymax = elems[3]
-            cls = elems[4]
-            if cls not in classes:
-                exit(0)
-            cls_id = classes.index(cls)
-            print(elems[0])
-            #
-            img_path = str('%s/Images/%s/%s.jpg'%(wd, project, os.path.splitext(txt_name)[0]))
-            #t = magic.from_file(img_path)
-            #wh= re.search('(\d+) x (\d+)', t).groups()
-            im=Image.open(img_path)
-            w= int(im.size[0])
-            h= int(im.size[1])
-            #w = int(xmax) - int(xmin)
-            #h = int(ymax) - int(ymin)
-            # print(xmin)
-            print(w, h)
-            print(float(xmin), float(xmax), float(ymin), float(ymax))
-            b = (float(xmin), float(xmax), float(ymin), float(ymax))
-            bb = convert((w,h), b)
-            print(bb)
-            txt_outfile.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
-
-    """ Save those images with bb into list"""
-    if(ct != 0):
-        list_file.write('%s/images/%s/%s.jpg\n'%(wd, cls, os.path.splitext(txt_name)[0]))
+            if(len(elems) >= 2):
+            #if(len(line) >= 2):
+                ct = ct + 1
+                print(line)
+                elems = line.split(' ')
+                print(elems)
+                xmin = elems[0]
+                xmax = elems[2]
+                ymin = elems[1]
+                ymax = elems[3]
+                cls = elems[4]
+                if cls not in classes:
+                    exit(0)
+                cls_id = classes.index(cls)
+                print(elems[0])
+                #
+                img_path = str('%s/Images/%s/%s.jpg'%(wd, project, os.path.splitext(txt_name)[0]))
+                #t = magic.from_file(img_path)
+                #wh= re.search('(\d+) x (\d+)', t).groups()
+                im=Image.open(img_path)
+                w= int(im.size[0])
+                h= int(im.size[1])
+                #w = int(xmax) - int(xmin)
+                #h = int(ymax) - int(ymin)
+                # print(xmin)
+                print(w, h)
+                print(float(xmin), float(xmax), float(ymin), float(ymax))
+                b = (float(xmin), float(xmax), float(ymin), float(ymax))
+                bb = convert((w,h), b)
+                print(bb)
                 
-list_file.close()       
+                txt_outfile.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+    
+        txt_outfile.close()
+        txt_file.close()
+        
+        """ Save those images with bb into list"""
+        if(ct != 0):
+            list_file.write('%s/images/%s/%s.jpg\n'%(wd, cls, os.path.splitext(txt_name)[0]))
+        else :
+            os.remove(txt_outpath)
+        
+        print ("\n")
+                    
+    list_file.close()   
+
+if __name__ == '__main__':
+    mypath = "./Result/yeongju/"
+    outpath = "./Result_YOLO/yeongju/"
+    project = "yeongju"
+    classes = ["pothole","patchdamaged","spalling"]
+    Convert2Yolo(mypath, outpath, project, classes)  
